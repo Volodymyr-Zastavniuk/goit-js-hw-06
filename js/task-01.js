a75d273bd63ca44e5977d1bfc7dde2702c29c5fe
Volodymyr-Zastavniuk <106408963+Volodymyr-Zastavniuk@users.noreply.github.com>
2022-10-05 01:36:44 +0300

    add task 1 and 2

---
+++ js/task-01.js
@@ -0,0 +1,25 @@
+// Напиши скрипт который:
+// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
+// В результате, в консоли будут выведены такие сообщения.
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+const categoriesListRef = document.querySelector("ul#categories");
+console.log(`Number of categories: ${categoriesListRef.childElementCount}`);
+
+const itemsListRef = categoriesListRef.querySelectorAll("li.item");
+itemsListRef.forEach((item) => {
+  console.log("");
+  console.log(`Category: ${item.querySelector("h2").textContent}`);
+  console.log(`Elements: ${item.querySelector("ul").childElementCount}`);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,28 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+// Напиши скрипт, который для каждого элемента массива ingredients:
+// Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+// Добавит название ингредиента как его текстовое содержимое.
+// Добавит элементу класс item.
+// После чего вставит все <li> за одну операцию в список ul#ingredients.
+
+const ingredientsContainerRef = document.querySelector("ul#ingredients");
+
+const createIngredientsRef = (ingredients) => {
+  return ingredients.map((ingredient) => {
+    const item = document.createElement("li");
+    item.textContent = ingredient;
+    item.classList.add("item");
+
+    return item;
+  });
+};
+
+ingredientsContainerRef.append(...createIngredientsRef(ingredients));
